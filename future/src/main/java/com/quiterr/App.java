52f9269370e41dff05ffdfc8b0b252f2124c3376
huangchen <huangchen@longshine.com>
2020-02-05 17:34:19 +0800

    future example

--- future/src/main/java/com/quiterr/App.java
+++ future/src/main/java/com/quiterr/App.java
@@ -5,11 +5,13 @@ import java.util.concurrent.*;
 import static java.lang.Thread.sleep;
 
 /**
- * Future代表异步计算的结果，这个接口提供了检查计算是否结束、等待计算、取消计算以及获取计算结果的方法。
- * 如果计算没有结束，get方法会阻塞。FutureTask是Future和Runnable的实现。
+ * {@link Future} 代表异步计算的结果，这个接口提供了检查计算是否结束、等待计算、取消计算以及获取计算结果的方法。
+ * 如果计算没有结束，get方法会阻塞。{@link FutureTask}是Future和Runnable的实现。
  *
+ * <pre>
  * FutureTask<String> futureTask = new FutureTask<>(()-> searcher.search("keyword"));
  * executor.execute(futureTask);
+ * </pre>
  *
  * @author huangchen
  * @date 2020/2/5
